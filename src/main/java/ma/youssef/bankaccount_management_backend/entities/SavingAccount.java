91127a398939a130356af70d1885a7bdef6f6d7c
IdrissiAymen <gilnashidrissi@gmail.com>
2026-01-02 13:14:50 +0100

    Initial commit

---
+++ src/main/java/ma/youssef/bankaccount_management_backend/entities/SavingAccount.java
@@ -0,0 +1,14 @@
+package ma.youssef.bankaccount_management_backend.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("SA")
+@Data @AllArgsConstructor @NoArgsConstructor
+public class SavingAccount extends BankAccount{
+    private double interestRate;
+}
